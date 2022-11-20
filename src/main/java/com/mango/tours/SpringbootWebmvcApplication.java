de01ddcab632a1e01b3b70c735b495dd1360aba1
dileepgit85 <dileep.mavidi@gmail.com>
2022-11-20 07:55:22 +0530

    added me new spring boot web mvc projecy

---
+++ src/main/java/com/mango/tours/SpringbootWebmvcApplication.java
@@ -0,0 +1,13 @@
+package com.mango.tours;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWebmvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mango/tours/SpringbootWebmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mango.tours;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
